f1cdad082affb351473d5b8c19ff8ce44495cc19
sanada ryo <r.sanada@tmdigital.jp>
2024-05-17 19:49:58 +0900

    test

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,8 +1,10 @@
 use lambda_runtime::{
-    //service_fn, 
-    //LambdaEvent, 
+    service_fn, 
+    LambdaEvent, 
     Error};
 //use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
+use tokio;
 
 #[tokio::main]
 async fn main() -> Result<(), Error>{
