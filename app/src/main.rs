b65979d342043b1ce9d026c3d4ded2dc0f981674
sanada ryo <r.sanada@tmdigital.jp>
2024-05-17 19:59:45 +0900

    テスト

--- app/src/main.rs
+++ app/src/main.rs
@@ -3,10 +3,24 @@ use lambda_runtime::{
     LambdaEvent, 
     Error};
 //use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
 use tokio;
 
+// #[tokio::main]
+// async fn main() -> Result<(), Error>{
+//     println!("Hello, world!!");
+//     Ok(json!({ "message": "Hello, world!!", "input": event }))
+// }
+
 #[tokio::main]
-async fn main() -> Result<(), Error>{
+async fn main() -> Result<(), Error> {
+    let func = service_fn(my_handler);
+    lambda_runtime::run(func).await?;
+    Ok(())
+}
+
+async fn my_handler(event: LambdaEvent<Value>) -> Result<Value, Error> {
+    let (event, _context) = event.into_parts();
     println!("Hello, world!!");
     Ok(json!({ "message": "Hello, world!!", "input": event }))
 }
