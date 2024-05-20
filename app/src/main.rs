93077a43d1465ef94c702094be67727534cf9347
k-matsuo-tcdigital <k.matsuo@tcdigital.jp>
2024-05-20 09:54:18 +0000

    オリジナルのリクエストとレスポンスの型定義とメイン処理関数を追加

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,11 +1,31 @@
-use lambda_runtime::{
-    //service_fn, 
-    //LambdaEvent, 
-    Error};
-//use serde::{Deserialize, Serialize};
+use lambda_runtime::{ 
+    service_fn,
+    Error,
+    LambdaEvent
+};
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize)]
+struct Request {
+    first_name: String,
+}
+
+#[derive(Serialize)]
+struct Response {
+    message: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Error>{
-    println!("Hello, world!!");
-    Ok(())
+    lambda_runtime::run(service_fn(handler)).await
+}
+
+async fn handler(event: LambdaEvent<Request>) -> Result<Response,Error> {
+    let first_name = event.payload.first_name;
+    let respons = Response {
+        message: format!("Hello, {first_name}!")
+    };
+    
+    Ok(respons)
+
 }
