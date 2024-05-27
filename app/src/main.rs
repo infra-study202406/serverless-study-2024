f85c4ee9777aff0451ae03bf55d453fa621fbda0
k-matsuo-tcdigital <k.matsuo@tcdigital.jp>
2024-05-27 05:43:05 +0000

    mainの処理で残っていたものを削除して整理

--- app/src/main.rs
+++ app/src/main.rs
@@ -6,9 +6,3 @@ use app::handler;
 async fn main() -> Result<(), Error> {
     lambda_runtime::run(service_fn(handler::lamdba_handler)).await
 }
-
-async fn my_handler(event: LambdaEvent<Value>) -> Result<Value, Error> {
-    let (event, _context) = event.into_parts();
-    println!("Hello, world!!");
-    Ok(json!({ "message": "Hello, world!!", "input": event }))
-}
